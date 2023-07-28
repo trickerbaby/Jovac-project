e1f234c0db5c991a2c3047766f53ad73e4ba6ad7
trickerbaby <nnnanu2@gmail.com>
2023-07-28 22:53:25 +0530

    third commuit

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 const express = require('express');
 const app = express();
-const port = 3000; 
+const port = 3000; // Replace with your desired port number
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let storedData = {}; 
+let quotesData = []; // To store the received data as an array of objects
 
-
+// Endpoint to receive the form data and store it
 app.post('/submit', (req, res) => {
   console.log('Received form data:', req.body);
 
- 
-  storedData = req.body;
+  // Save the received data in the quotesData array
+  quotesData.push(req.body);
 
   res.sendStatus(200);
 });
 
-
+// Endpoint to serve the index.html page
 app.get('/index.html', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+// Endpoint to send the quotesData as JSON to the client
 app.get('/data', (req, res) => {
-  res.json(storedData);
+  res.json(quotesData);
 });
 
 app.listen(port, () => {
